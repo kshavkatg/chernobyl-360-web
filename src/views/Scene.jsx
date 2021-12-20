72376872e65f9a0345dcb97af1f35c0e9da3b0e0
Shavkat <kshavkatg@gmail.com>
2021-12-20 15:12:16 +0600

    addd loading view and custom buttons with start view

--- src/views/Scene.jsx
+++ src/views/Scene.jsx
@@ -1,101 +1,104 @@
 import { useEffect, useState } from 'react'
 import * as AFRAME from 'aframe'
+import 'aframe'
+import LoadingView from './LoadingView'
+import StartView from './StartView'
 
-AFRAME.registerComponent('play-on-window-click', {
-  init: function () {
-    this.onClick = this.onClick.bind(this);
-  },
-  play: function () {
-    window.addEventListener('click', this.onClick);
-  },
-  pause: function () {
-    window.removeEventListener('click', this.onClick);
-  },
-  onClick: function (evt) {
-    var video = this.el.components.material.material.map.image;
-    if (!video) { return; }
-    video.play();
-  }
-});
+// AFRAME.registerComponent('play-on-window-click', {
+//   init: function () {
+//     this.onClick = this.onClick.bind(this);
+//   },
+//   play: function () {
+//     window.addEventListener('click', this.onClick);
+//   },
+//   pause: function () {
+//     window.removeEventListener('click', this.onClick);
+//   },
+//   onClick: function (evt) {
+//     var video = this.el.components.material.material.map.image;
+//     if (!video) { return; }
+//     video.play();
+//   }
+// });
 
-AFRAME.registerComponent('play-on-vrdisplayactivate-or-enter-vr', {
-  init: function () {
-    this.playVideo = this.playVideo.bind(this);
-    this.playVideoNextTick = this.playVideoNextTick.bind(this);
-  },
-  play: function () {
-    window.addEventListener('vrdisplayactivate', this.playVideo);
-    this.el.sceneEl.addEventListener('enter-vr', this.playVideoNextTick);
-  },
-  pause: function () {
-    this.el.sceneEl.removeEventListener('enter-vr', this.playVideoNextTick);
-    window.removeEventListener('vrdisplayactivate', this.playVideo);
-  },
-  playVideoNextTick: function () {
-    setTimeout(this.playVideo);
-  },
-  playVideo: function () {
-    var video = this.el.components.material.material.map.image;
-    if (!video) { return; }
-    video.play();
-  }
-});
+// AFRAME.registerComponent('play-on-vrdisplayactivate-or-enter-vr', {
+//   init: function () {
+//     this.playVideo = this.playVideo.bind(this);
+//     this.playVideoNextTick = this.playVideoNextTick.bind(this);
+//   },
+//   play: function () {
+//     window.addEventListener('vrdisplayactivate', this.playVideo);
+//     this.el.sceneEl.addEventListener('enter-vr', this.playVideoNextTick);
+//   },
+//   pause: function () {
+//     this.el.sceneEl.removeEventListener('enter-vr', this.playVideoNextTick);
+//     window.removeEventListener('vrdisplayactivate', this.playVideo);
+//   },
+//   playVideoNextTick: function () {
+//     setTimeout(this.playVideo);
+//   },
+//   playVideo: function () {
+//     var video = this.el.components.material.material.map.image;
+//     if (!video) { return; }
+//     video.play();
+//   }
+// });
 
-AFRAME.registerComponent('arrow-key-rotation', {
-  schema: {
-    enabled: {default: true},
-    dx: {default: 2.0},
-    dy: {default: 2.0},
-  },
-  init: function () {
-    this.onKeyDown = this.onKeyDown.bind(this);
-    this.onKeyUp = this.onKeyUp.bind(this);
-    this.directionX = 0;
-    this.directionY = 0;
-  },
-  play: function () {
-    window.addEventListener('keydown', this.onKeyDown);
-    window.addEventListener('keyup', this.onKeyUp);
-  },
-  pause: function () {
-    window.removeEventListener('keydown', this.onKeyDown);
-    window.removeEventListener('keyup', this.onKeyUp);          
-  },
-  onKeyDown: function (evt) {
-    switch (evt.keyCode) {
-      case 37: this.directionX = 1; break;
-      case 38: this.directionY = 1; break;
-      case 39: this.directionX = -1; break;
-      case 40: this.directionY = -1; break;
-    }
-  },
-  onKeyUp: function (evt) {
-    switch (evt.keyCode) {
-      case 37: this.directionX = 0; break;
-      case 38: this.directionY = 0; break;
-      case 39: this.directionX = 0; break;
-      case 40: this.directionY = 0; break;
-    }          
-  },
-  tick: function (t, dt) {
-    if (!this.data.enabled) { return; }
-    var rotation = this.el.getAttribute('rotation');
-    if (!rotation) { return; }
-    if (this.directionX || this.directionY) {
-      rotation.x += this.data.dx * this.directionY;
-      rotation.y += this.data.dy * this.directionX;
-      this.el.setAttribute('rotation', rotation);
-    }
-  }
-});
+// AFRAME.registerComponent('arrow-key-rotation', {
+//   schema: {
+//     enabled: {default: true},
+//     dx: {default: 2.0},
+//     dy: {default: 2.0},
+//   },
+//   init: function () {
+//     this.onKeyDown = this.onKeyDown.bind(this);
+//     this.onKeyUp = this.onKeyUp.bind(this);
+//     this.directionX = 0;
+//     this.directionY = 0;
+//   },
+//   play: function () {
+//     window.addEventListener('keydown', this.onKeyDown);
+//     window.addEventListener('keyup', this.onKeyUp);
+//   },
+//   pause: function () {
+//     window.removeEventListener('keydown', this.onKeyDown);
+//     window.removeEventListener('keyup', this.onKeyUp);          
+//   },
+//   onKeyDown: function (evt) {
+//     switch (evt.keyCode) {
+//       case 37: this.directionX = 1; break;
+//       case 38: this.directionY = 1; break;
+//       case 39: this.directionX = -1; break;
+//       case 40: this.directionY = -1; break;
+//     }
+//   },
+//   onKeyUp: function (evt) {
+//     switch (evt.keyCode) {
+//       case 37: this.directionX = 0; break;
+//       case 38: this.directionY = 0; break;
+//       case 39: this.directionX = 0; break;
+//       case 40: this.directionY = 0; break;
+//     }          
+//   },
+//   tick: function (t, dt) {
+//     if (!this.data.enabled) { return; }
+//     var rotation = this.el.getAttribute('rotation');
+//     if (!rotation) { return; }
+//     if (this.directionX || this.directionY) {
+//       rotation.x += this.data.dx * this.directionY;
+//       rotation.y += this.data.dy * this.directionX;
+//       this.el.setAttribute('rotation', rotation);
+//     }
+//   }
+// });
 
 function AFrameScene() {
 
   return (
     <a-scene
-      vr-mode-ui="enabled: true"
       loading-screen="enabled: false"
       device-orientation-permission-ui="enabled: false"
+      vr-mode-ui="enterVRButton: #myEnterVRButton; enterARButton: #myEnterARButton"
     >
       
       <a-assets>
@@ -120,7 +123,12 @@ function AFrameScene() {
       play-on-window-click
       play-on-vrdisplayactivate-or-enter-vr>
       </a-videosphere>
-      
+      <div className='buttons'>
+        <div id="myEnterVRButton" >
+        </div>
+        <div id="myEnterARButton" >
+        </div>
+      </div>
     </a-scene>
   )
 }
@@ -152,9 +160,10 @@ export const Scene = ({onLoaded, onReady}) => {
   return (
     <>
       {!isLoaded &&
-        <h1>loading</h1>
+        <LoadingView />
       }
       <AFrameScene />
+      <StartView />
     </>
   )
 }
