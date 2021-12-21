586608bf00f8c9e5508d9bc18b0afe1116d1ba64
Shavkat <kshavkatg@gmail.com>
2021-12-21 15:10:28 +0600

    test add sty

--- src/views/Scene.jsx
+++ src/views/Scene.jsx
@@ -119,6 +119,9 @@ function AFrameScene() {
           <source type="video/mp4"
                src="https://dm0qx8t0i9gc9.cloudfront.net/watermarks/video/R5WCqjT/360-video-maines-majestic-rocky-coast_Sq1wL45s__0d0aff323909ccf9a5c83f11b7808059__P640.mp4" />
         </video>
+        
+        <img id="background1" src="https://cdn.eso.org/images/publicationjpg/ESO_Paranal_360_Marcio_Cabral_Chile_09-CC.jpg" crossOrigin="anonymous"></img>
+
         <img id="observatory" src="https://cdn.eso.org/images/screen/ESO_Paranal_360_Marcio_Cabral_Chile_05-CC.jpg" crossOrigin="anonymous"></img>
         <img id="observatory2" src="https://cdn.eso.org/images/screen/ESO_Paranal_360_Marcio_Cabral_Chile_16-CC.jpg" crossOrigin="anonymous"></img>
         <img id="bordeauxtheater" src="https://cdn.glitch.com/f2ee9d74-9726-4bed-9c30-2d49d6391dee%2FOp%C3%A9ra_National_de_Bordeaux%20(1).jpg?1547235572572" crossOrigin="anonymous"></img>
@@ -137,11 +140,13 @@ function AFrameScene() {
         
       </a-camera>
       
-      <a-sphere id="sphere" material="src: #observatory" className="cantap" intersect position="-3 0 -5"></a-sphere>
-      <a-sphere id="sphere1" material="src: #observatory2" className="cantap" intersect position="-0 0 -5"></a-sphere>
-      <a-sphere id="sphere2" material="src: #bordeauxtheater" className="cantap" intersect position="3 0 -5"></a-sphere>
+      <a-sphere id="sphere" material="src: #observatory" className="cantap" intersect position="-5 2 -6"></a-sphere>
+      <a-sphere id="sphere1" material="src: #observatory2" className="cantap" intersect position="-0 2 -6"></a-sphere>
+      <a-sphere id="sphere2" material="src: #bordeauxtheater" className="cantap" intersect position="5 2 -6 "></a-sphere>
       
-      <a-videosphere rotation="0 180 0" src="#video">
+      <a-sky src="background1" rotation="0 -130 0"></a-sky>
+
+      <a-videosphere visible="false" rotation="0 180 0" src="#video">
       </a-videosphere>
 
       <div className='buttons'>
